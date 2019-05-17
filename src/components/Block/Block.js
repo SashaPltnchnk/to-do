08b1916b50cabea73559b63c58d51e833bd3400b
ZDubina <ZDubinaQ@gmail.com>
2019-05-17 17:22:56 +0300

    added axios and firebase

--- src/components/Block/Block.js
+++ src/components/Block/Block.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import Task from './Task/Task';
+import axios from '../../axios-todos';
 
 
 class Block extends Component {
+    state = {
+        showTask: true
+    }
+
+    deleteTask = () => {
+        this.setState({ showTask: false })
+    }
+
+    componentDidMount() {
+        axios.get('tasks.json')
+            .then( res => {
+                console.log(res);
+            })
+            .catch( err => {
+                console.error(err);
+            })
+    }
+
   render() {
     return (
       <div>
-        <Task/>
-        <Task/>
-        <Task/>
-        <Task/>
+        {
+            this.state.showTask ? <Task delete={this.deleteTask} /> : null
+        }
       </div>
     )
   }
--- src/components/Block/Task/Task.js
+++ src/components/Block/Task/Task.js
@@ -3,8 +3,12 @@ import React from 'react';
 import classes from './Task.module.css';
 
 const task = (props) => {
+    console.log(props.delete)
     return (
         <div className={classes.Task}>
+            <div className={classes.Delete}
+                onClick={props.delete}
+                >del</div>
             <h3 className={classes.Title}>Buy</h3>
             <div>milk, meat, butter</div>
         </div>
