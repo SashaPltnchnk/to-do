75447b940e52dbae18b4faf3afa4e75482debb3a
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-07-02 20:43:32 +0300

    fixed add card input

--- src/components/Column/AddCard.js
+++ src/components/Column/AddCard.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { addTask, fetchTasks } from '../../store/actions/task'
+import { connect } from 'react-redux'
 
 import classes from './Column.module.css'
 
-export default function AddCard() {
-    return (
-        <form className={classes.AddCard}>
-            <input placeholder="Add Card.." className={classes.AddList}/>
-        </form>
-    )
+class AddCard extends Component{
+    state = {
+        body: '',
+        title: ''
+      }
+    
+      changeInput = (e) => {
+        const {name, value} = e.target
+        this.setState(state => {
+            return {
+                ...state,
+                [name]: value
+            }
+        })
+      }
+    
+
+    submitHandler = e => {
+        e.preventDefault();
+        this.props.addTask({title: this.state.title})
+          .then(() => {
+              console.warn("allo")
+            this.setState({title: ''});
+            this.props.fetchTasks();
+              
+            })
+    }
+
+    render() {
+        return (
+            <form className={classes.AddCard} onSubmit={this.submitHandler}>
+                <input 
+                    placeholder="Add Card.." 
+                    type="text" 
+                    name="title" 
+                    value={this.state.title}
+                    onChange={this.changeInput}
+                    className={classes.AddList}/>
+            </form>
+        )
+    }
+    
 }
+
+const mapDispatchToProps = { addTask, fetchTasks };
+
+export default connect(null, mapDispatchToProps)(AddCard)
